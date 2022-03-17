f36be5863feced94471e9d50d64deeec483fa52d
junjie2712 <62163334+junjie2712@users.noreply.github.com>
2022-03-17 15:39:54 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cuxnebfeu6fk.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = leejunjie-employee"
+customregion = "us-east-1"
